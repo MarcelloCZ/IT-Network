28c60fc26e5acb9510ae657f6df581279b124bc2
Dominik Čelechovský <129881848+MarcelloCZ@users.noreply.github.com>
2023-10-22 20:43:56 +0200

    Projekt

---
+++ Projekt-EvidencePojistencu-master/ConsoleApp1/Evidence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Evidence
+    {
+
+        public List<Pojistenec> Pojistenci = new List<Pojistenec>();
+
+        
+        public void PridejPojistence(Pojistenec pojistenec)
+        {
+            Pojistenci.Add(pojistenec); 
+        }
+
+        
+        public void Vyhledat(string jmeno, string prijmeni)
+        {
+            foreach (var item in Pojistenci)
+            {
+                if(item.Jmeno == jmeno && item.Prijmeni == prijmeni)
+                {
+                    Console.WriteLine(item);
+                }
+                
+            }
+        }
+
+       
+        public void Vypis()
+        {
+            foreach (var item in Pojistenci)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        
+    }
+}
---
+++ Projekt-EvidencePojistencu-master/ConsoleApp1/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Evidence pojištěných");
+            Console.WriteLine("--------------------");
+
+           
+            char volba = '0';
+            Evidence evidence = new Evidence();
+
+            
+            while (volba != '4')
+            {
+               
+                Console.WriteLine("Vyberte si akci:");
+                Console.WriteLine("1 - Přidat nového pojištěného");
+                Console.WriteLine("2 - Vypsat všechny pojištěné");
+                Console.WriteLine("3 - Vyhledat pojištěného");
+                Console.WriteLine("4 - Konec");
+                volba = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (volba)
+                {
+                    case '1':
+                        
+                        Console.WriteLine("Zadejte jméno pojištěného:");
+                        string jmeno;
+                        while(string.IsNullOrWhiteSpace(jmeno = Console.ReadLine()))
+                        {
+                            Console.WriteLine("Zadej znovu.");
+                        }
+                        string prijmeni;
+                        Console.WriteLine("Zadejte příjmení pojištěného:");
+                        while(string.IsNullOrWhiteSpace(prijmeni = Console.ReadLine()))
+                        {
+                            Console.WriteLine("Zadej znovu.");
+                        }
+
+                      
+                        Console.WriteLine("Zadej věk pojištěného:");
+                        int vek;
+                        while (!int.TryParse(Console.ReadLine(), out vek))
+                        {
+                            Console.WriteLine("Zadej prosím číslo.");
+                        }
+                       
+                        Console.WriteLine("Zadej telefonní číslo pojištěného:");
+                        int telefon;
+                        while (!int.TryParse(Console.ReadLine(), out telefon))
+                        {
+                            Console.WriteLine("Zadej prosím číslo.");
+                        }
+                        
+                        
+                        Pojistenec pojistenec = new Pojistenec(jmeno,prijmeni,telefon,vek);
+                        evidence.PridejPojistence(pojistenec);
+                        Console.WriteLine("Data byla uložena. Pokračujte libovolnou klávesou ...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case '2':
+                        
+                        Console.WriteLine("Výpis pojištěných");
+                        Console.WriteLine("--------------------");
+                        evidence.Vypis();
+                        Console.WriteLine();
+                        Console.WriteLine("Pokračujte libovolnou klávesou ...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case '3':
+                      
+                        Console.WriteLine("Zadejte jméno pojištěného:");
+                        string jmenoSearch = Console.ReadLine();
+                        Console.WriteLine("Zadejte příjmení pojištěného:");
+                        string prijmeniSearch = Console.ReadLine();
+                        Console.WriteLine("Evidence nalezených pojištěných:");
+                        Console.WriteLine("--------------------");
+                        
+                        evidence.Vyhledat(jmenoSearch, prijmeniSearch);
+                        Console.WriteLine();
+                        Console.WriteLine("Pokračujte libovolnou klávesou ...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case '4':
+                        Console.WriteLine("Děkuji za použití programu.");
+                        Console.ReadKey();
+                        break;   
+                    default:
+                        Console.WriteLine("Neplatná volba, zadejte prosím znovu.");
+                        break;
+                }
+            }
+        
+
+
+
+
+
+
+        }
+    }
+}
